b8a84d74d6342c023a2b19c0c1004a57e0de766a
Johnny Shumway <shumwayjohnny@gmail.com>
2025-10-27 03:53:05 -0400

    Add documentation and author details to Constants.py
    Added docstring and author information to Constants.py

--- Constants.py
+++ Constants.py
@@ -1,3 +1,15 @@
+"""
+POD Constants
+Author: Johnny Shumway (jShum00), adapted from Dummiesman.
+
+This is a data class used by PyPOD, it is based on the data found in Dummiesman's
+Constants.cs for his Poddy program, this is used for find header and entry data for
+the extractor.
+
+https://github.com/Dummiesman/Poddy/tree/main/PODTool
+"""
+
+@dataclass
 class Constants:
     # ---- EPD1 ----
     EPD1_HEADER_SIZE = 0x110
